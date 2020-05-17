62b5140441b8c2a362f8dfdd045b45b4f81d8fd8
haizhou85 <haizhou.guo@gmail.com>
2020-05-17 02:13:02 -0700

    Update

--- src/main/java/com/msr/data/SiteRepository.java
+++ src/main/java/com/msr/data/SiteRepository.java
@@ -14,9 +14,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface SiteRepository extends PagingAndSortingRepository<Site, Integer> {
-    
-    @Query("SELECT SUM(SIZE_SQFT) FROM SITE_USES WHERE SITE_ID = ?1")
-    public long total_size(int site_id);
 }
 
 ////////////////////////////////////////////////////////////
